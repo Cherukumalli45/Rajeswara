f6af0a8b8ddafc68c9032ef2466a017b60162140
Cherukumalli45 <chrjc97@gmail.com>
2025-04-25 16:35:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0" 
+    ami           = "ami-060a84cbcb5c14844" 
     instance_type = "t2.micro"
+    subnet_id = "subnet-03aff6313cc36b3ac"
+    key_name = "april25"
 }
